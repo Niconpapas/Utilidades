cb8d986384235eed46489e508a6a4eafe35703f2
NicoLaptop <fnc1001@gmail.com>
2018-03-11 15:57:31 -0300

    Added utilidades

---
+++ Utilidades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Application
+{
+	public class Utilidades:Page
+	{
+		public MsgBox(string pMensaje)
+		{
+			string msj="<script languaje='Javascript'> alert('"+pMensaje+"');</script>";
+			Page.ClientScript.RegisterStartupScript(Page.GetType(), "mensaje", msj);
+		}
+	}
+}
+
